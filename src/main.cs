f3a1bc9408e07c78cd6ffb7e451c50d85d1958a2
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-12-02 23:06:49 +0700

    console

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,9 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+        // Wait for user input
+        var command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found");
     }
 }
