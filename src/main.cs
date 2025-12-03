374192a9ff1c357adaaa553369619a2c240caee8
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-12-03 22:43:38 +0700

    update code

--- src/main.cs
+++ src/main.cs
@@ -1,58 +1,64 @@
-string[] BUILTIN_CMDS = ["echo", "type", "exit"];
-
-while (true)
+class Program
 {
-    Console.Write("$ ");
-    // Wait for user input
-    var inputStr = Console.ReadLine();
-
-    if (inputStr == "exit 0")
+    static void Main()
     {
-        Environment.Exit(0);
-    }
+        string[] BUILTIN_CMDS = ["echo", "type", "exit"];
 
-    var parts = inputStr.Split();
-    var cmd = parts[0];
-    var inputArgs = parts[1..];
+        while (true)
+        {
+            Console.Write("$ ");
+            // Wait for user input
+            var inputStr = Console.ReadLine();
 
-    switch (cmd)
-    {
-        case "echo":
-            Console.WriteLine(String.Join(" ", inputArgs));
-            break;
-        case "type":
-            var inputArg = inputArgs[0];
-
-            var isBuiltinCmd = BUILTIN_CMDS.Contains(inputArg);
-            if (isBuiltinCmd)
+            if (inputStr == "exit 0")
             {
-                Console.WriteLine($"{inputArg} is a shell builtin");
+                Environment.Exit(0);
             }
-            else
+
+            var parts = inputStr.Split();
+            var cmd = parts[0];
+            var inputArgs = parts[1..];
+
+            switch (cmd)
             {
-                var pathsStr = Environment.GetEnvironmentVariable("PATH");
-                var pathsArr = pathsStr.Split(":");
-                bool isFound = false;
-                foreach (var path in pathsArr)
-                {
-                    var joinedPath = Path.Join(path, inputArg);
-                    if (File.Exists(joinedPath))
+                case "echo":
+                    Console.WriteLine(String.Join(" ", inputArgs));
+                    break;
+                case "type":
+                    var inputArg = inputArgs[0];
+
+                    var isBuiltinCmd = BUILTIN_CMDS.Contains(inputArg);
+                    if (isBuiltinCmd)
                     {
-                        isFound = true;
-                        Console.WriteLine($"{inputArg} is {joinedPath}");
-                        break;
+                        Console.WriteLine($"{inputArg} is a shell builtin");
                     }
-                }
+                    else
+                    {
+                        var pathsStr = Environment.GetEnvironmentVariable("PATH");
+                        var pathsArr = pathsStr.Split(":");
+                        bool isFound = false;
+                        foreach (var path in pathsArr)
+                        {
+                            var joinedPath = Path.Join(path, inputArg);
+                            if (File.Exists(joinedPath))
+                            {
+                                isFound = true;
+                                Console.WriteLine($"{inputArg} is {joinedPath}");
+                                break;
+                            }
+                        }
 
-                if (!isFound)
-                {
-                    Console.WriteLine($"{inputArg}: not found");
-                }
+                        if (!isFound)
+                        {
+                            Console.WriteLine($"{inputArg}: not found");
+                        }
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine($"{inputStr}: command not found");
+                    break;
             }
-
-            break;
-        default:
-            Console.WriteLine($"{inputStr}: command not found");
-            break;
+        }
     }
 }
