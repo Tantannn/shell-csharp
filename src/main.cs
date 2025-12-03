be6426f1895739bd6719d4ebf4ce4afd93cfa2d3
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-12-03 22:18:08 +0700

    update code

--- src/main.cs
+++ src/main.cs
@@ -2,6 +2,7 @@ class Program
 {
     // 1. Define a dictionary to map command names (strings) to Actions
     static readonly Dictionary<string, Action<string[]>> commands = new();
+
     static void Main()
     {
         // 2. Register your commands here
@@ -48,6 +49,7 @@ class Program
 
             var targetCmd = args[0];
 
+
             // Check if the command is in our dictionary or is a special keyword
             if (commands.ContainsKey(targetCmd) || targetCmd == "exit")
             {
@@ -55,8 +57,22 @@ class Program
             }
             else
             {
-                // In a real shell, you would check the PATH environment variable here
-                Console.WriteLine($"{targetCmd}: not found");
+                var pathsStr = Environment.GetEnvironmentVariable("PATH");
+                var pathsArr = pathsStr.Split(":");
+                var isFound = false;
+                foreach (var path in pathsArr)
+                {
+                    var joinedPath = Path.Join(path, targetCmd);
+                    if (!File.Exists(joinedPath)) continue;
+                    isFound = true;
+                    Console.WriteLine($"{targetCmd} is {joinedPath}");
+                    break;
+                }
+
+                if (!isFound)
+                {
+                    Console.WriteLine($"{targetCmd}: not found");
+                }
             }
         });
 
