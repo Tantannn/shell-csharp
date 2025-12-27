c2818fb3896a19e2fadf974f8bcadae75f2d472f
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-12-27 19:57:43 +0700

    update code

--- src/AutoCompletionHandler.cs
+++ src/AutoCompletionHandler.cs
@@ -4,18 +4,20 @@ using System.IO;
 using System.Linq;
 namespace Commands.Helpers {
   public class AutoCompletionHandler : IAutoCompleteHandler {
-    public char[] Separators { get; set; }
+    public char[] Separators { get; set; } = "abcdefghijklmnopqrstuvwxyz".ToArray();
 
     private string[] commands = ["echo", "exit"];
 
-    public AutoCompletionHandler() {
-      Separators = "abcdefghijklmnopqrstuvwxyz".ToArray();
-    }
-
     public string[] GetSuggestions(string text, int index) {
-      return commands.Where(c => c.StartsWith(text))
+      var matches =  commands.Where(c => c.StartsWith(text))
           .Select(c => c.Substring(text.Length) + " ")
           .ToArray();
+      if (matches.Length == 0)
+      {
+        Console.Write('\a');
+        return [];
+      }
+      return matches;
     }
   }
 }
